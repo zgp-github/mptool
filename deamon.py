df6474c0e7fa20272ccf51c3bcb4cb69929badd1
zgp-github <zgpgoole@gmail.com>
2018-06-07 22:02:43 +0800

    add read the database data

--- deamon.py
+++ deamon.py
@@ -10,6 +10,9 @@ from PyQt5.QtCore import QEvent, QTimer
 import threading
 from threading import Timer
 from time import *
+import sqlite3
+import requests
+import json
 
 class Dialog(QDialog):
     test_1_running = False
@@ -32,7 +35,6 @@ class Dialog(QDialog):
         self.create_cmd_input()
         self.create_test_result_show()
         self.create_info_show()
-        self.create_auto_test()
 
         buttonBox = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
         # buttonBox.accepted.connect(self.accept)
@@ -44,7 +46,6 @@ class Dialog(QDialog):
         mainLayout.addWidget(self.formGroupBox)
         mainLayout.addWidget(self.QGroupBox_info_show)
         mainLayout.addWidget(buttonBox)
-        mainLayout.addWidget(self.horizontalGroupBox)
         self.setLayout(mainLayout)
 
         # It is a timer test code
@@ -52,6 +53,7 @@ class Dialog(QDialog):
         self.timer.setInterval(1000)
         self.timer.start()
         self.timer.timeout.connect(self.onTimerOut)
+        self.daemon_start()
 
     def onTimerOut(self):
         self.bigEditor.append("timer test...")
@@ -68,41 +70,41 @@ class Dialog(QDialog):
         self.exitAction.triggered.connect(self.accept)
 
     def create_cmd_input(self):
-        self.gridGroupBox = QGroupBox("命令输入区")
+        self.gridGroupBox = QGroupBox("数据显示")
         layout = QGridLayout()
 
-        table = QTableWidget(3, 2)
+        self.table = QTableWidget(3, 2)
         # auto adapt the width
-        table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
         # set canot edit the table data
-        table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.table.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
-        table.setHorizontalHeaderLabels(['类型', '数据'])
+        self.table.setHorizontalHeaderLabels(['类型', '数据'])
         font = QFont("Microsoft YaHei", 10)
-        table.setFont(font)
+        self.table.setFont(font)
 
         newItem = QTableWidgetItem("MAC地址")
-        table.setItem(0, 0, newItem)
+        self.table.setItem(0, 0, newItem)
         newItem = QTableWidgetItem("None")
-        table.setItem(0, 1, newItem)
+        self.table.setItem(0, 1, newItem)
 
         newItem = QTableWidgetItem("传感器类型")
-        table.setItem(1, 0, newItem)
+        self.table.setItem(1, 0, newItem)
         newItem = QTableWidgetItem("None")
-        table.setItem(1, 1, newItem)
+        self.table.setItem(1, 1, newItem)
 
         newItem = QTableWidgetItem("传感器状态")
-        table.setItem(2, 0, newItem)
+        self.table.setItem(2, 0, newItem)
         newItem = QTableWidgetItem("None")
-        table.setItem(2, 1, newItem)
+        self.table.setItem(2, 1, newItem)
 
-        layout.addWidget(table, 0, 2, 4, 1)
+        layout.addWidget(self.table, 0, 2, 4, 1)
         layout.setColumnStretch(2, 100)
         self.gridGroupBox.setLayout(layout)
 
     def create_test_result_show(self):
-        self.formGroupBox = QGroupBox("测试结果")
+        self.formGroupBox = QGroupBox("数据上传结果")
         layout = QFormLayout()
 
         self.test_result = QLabel(self, text="测试结果")
@@ -118,13 +120,13 @@ class Dialog(QDialog):
     def update_test_resule_show(self, status='None'):
         print("update_test_resule_show: "+status)
         if status == "success":
-            info = "测试通过"
+            info = "数据上传成功"
             self.test_result.setStyleSheet('''color: black; background-color: green''')
         elif status == "fail":
-            info = '测试失败'
+            info = '数据上传失败'
             self.test_result.setStyleSheet('''color: black; background-color: red''')
         else:
-            info = '请开始测试'
+            info = '等待数据中'
             self.test_result.setStyleSheet('''color: black; background-color: gray''')
         self.test_result.setText(info)
         self.test_result.setFont(QFont("Microsoft YaHei", 20))
@@ -144,65 +146,17 @@ class Dialog(QDialog):
         layout.addRow(self.bigEditor)
         self.QGroupBox_info_show.setLayout(layout)
 
-    def create_auto_test(self):
-        self.horizontalGroupBox = QGroupBox("功能自动测试")
-        layout = QHBoxLayout()
-
-        button_1 = QPushButton("自动测试 1")
-        button_1.clicked.connect(self.auto_test_1)
-        layout.addWidget(button_1)
-
-        button_2 = QPushButton("自动测试 2")
-        button_2.clicked.connect(self.auto_test_2)
-        layout.addWidget(button_2)
-
-        button_3 = QPushButton("自动测试 3")
-        button_3.clicked.connect(self.auto_test_3)
-        layout.addWidget(button_3)
-
-        button_4 = QPushButton("自动测试 4")
-        button_4.clicked.connect(self.auto_test_4)
-        layout.addWidget(button_4)
-        
-        self.horizontalGroupBox.setLayout(layout)
-
-    def update_msg_show(self, id=None):
-        if id == 1:
-            msg = "请扫描配对命令码"
-        elif id == 2:
-            msg = "请按下传感器的配对按钮"
-        elif id == 3:
-            msg = "命令码错误"
-        else:
-            msg = "请扫描配对命令码"
-        self.msg_show.setText(msg)
-
-    def auto_test_1(self):
+    def daemon_start(self):
         if Dialog.test_1_running == False:
             Dialog.test_1_running = True
-            print("auto_test_1")
-            self.bigEditor.append("start auto test 1")
-            t = threading.Thread(target=self.test_unit_1)
+            self.bigEditor.append("daemon_start")
+            t = threading.Thread(target=self.get_FTS_data)
             t.start()
         else:
             self.bigEditor.append("test 1 is already running!")
             pass
 
-    def auto_test_2(self):
-        print("auto_test_2")
-        self.bigEditor.append("start auto test 2")
-
-    def auto_test_3(self):
-        print("auto_test_3")
-        self.bigEditor.append("start auto test 3")
-        self.update_test_resule_show("success")
-
-    def auto_test_4(self):
-        print("auto_test_4")
-        self.bigEditor.append("start auto test 4")
-        self.update_test_resule_show("fail")
-
-    def test_unit_1(self):
+    def get_FTS_data(self):
         while True:
             print("test_unit_1")
             self.bigEditor.append("success")
@@ -214,6 +168,19 @@ class Dialog(QDialog):
             self.update_test_resule_show()
             self.bigEditor.append("no statues")
             sleep(1)
+
+            db = "ftsTestResults.db"
+            conn = sqlite3.connect(db)
+            c = conn.cursor()
+            cmd = "SELECT TestID, TestLimitsID, TimeStamp, TestStatus, TestResult, FtsSerialNumber, ChipSerialNumber, MACAddress, DUTSerialNumber, RaceConfigID   from Tests"
+            cursor = c.execute(cmd)
+            for row in cursor:
+                print(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9])
+                mac = row[7]
+                newItem = QTableWidgetItem(mac)
+                self.table.setItem(0, 1, newItem)
+                self.bigEditor.append(mac)
+            conn.close()
 
 if __name__ == '__main__':
     import sys
