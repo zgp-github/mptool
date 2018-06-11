69b353ad47006e2dcdd79b4b0ec49b2fe59c442b
zgp-github <zgpgoole@gmail.com>
2018-06-11 17:00:21 +0800

    add parser database

--- mptool_pyqt5/fts_data.py
+++ mptool_pyqt5/fts_data.py
@@ -7,11 +7,33 @@ import requests
 import json
 
 class fts_data():
+    current_mac = None
+    current_id = None
     def __init__(self):
         print("it is a init function")
+        self.init_data()
+    
+    def init_data(self):
+        print("fts_data init data")
+        id_list = []
+        db = "ftsTestResults.db"
+        cmd = "SELECT TestID, TestLimitsID, TimeStamp, TestStatus, TestResult, FtsSerialNumber, ChipSerialNumber, MACAddress, DUTSerialNumber, RaceConfigID   from Tests"
+        conn = sqlite3.connect(db)
+        c = conn.cursor()
+        cursor = c.execute(cmd)
+        for row in cursor:
+            id_list.append(row[0])
+        self.current_id = max(id_list)
+        print("current_id:", self.current_id)
+
+        sql_cmd = "SELECT MACAddress FROM Tests WHERE  TestID = "+str(self.current_id)
+        cursor = c.execute(sql_cmd)
+        for row in cursor:
+            self.current_mac = row[0]
+            print("current_mac:", self.current_mac)
+        conn.close()
     
     def get_Tests_data(self):
-        data = []
         print("get the Tests table data")
         db = "ftsTestResults.db"
         cmd = "SELECT TestID, TestLimitsID, TimeStamp, TestStatus, TestResult, FtsSerialNumber, ChipSerialNumber, MACAddress, DUTSerialNumber, RaceConfigID   from Tests"
@@ -19,9 +41,9 @@ class fts_data():
         c = conn.cursor()
         cursor = c.execute(cmd)
         for row in cursor:
+            data = []
             data.append(row[0])
             data.append(row[7])
-            sleep(1)
             print("get fts data:", data)
-            return data
+        return data
         conn.close()
