0441f5135dfb688182c8cfd939872fd8ae626973
zgp-github <zgpgoole@gmail.com>
2018-06-12 10:35:05 +0800

    update net config read

--- mptool_pyqt5/net.py
+++ mptool_pyqt5/net.py
@@ -23,25 +23,9 @@ class network():
         conf = configparser.ConfigParser()
         if os.path.exists(config):
             print("config.ini exist")
-            pass
-        else:
-            print("config.ini not exist, create it")
             conf.read(config)
-            conf.add_section("Mptool4PC")
-            conf.set("Mptool4PC","STATION","PCBA_FTS")
-            conf.set("Mptool4PC","#station1","PCBA_FTS")
-            conf.set("Mptool4PC","#station2","ASSEMBLY_PAIR")
-            conf.set("Mptool4PC","#station3","ASSEMBLY_FTS")
-            conf.set("Mptool4PC","#station4","ASSEMBLY_ML1")
-            conf.set("Mptool4PC","#station5","ASSEMBLY_GCL")
-            conf.set("Mptool4PC","#station6","ASSEMBLY_REPAIR")
-            conf.set("Mptool4PC","TN4CIOIP","192.168.10.150")
-            conf.write(open(config, "w"))
-
-        conf.read(config)
-        station = conf.get('Mptool4PC', 'STATION')
-        tn4cioip = conf.get('Mptool4PC', 'TN4CIOIP')
-        print("--------------------------------------------------------------:",station, tn4cioip)
+            tn4cioip = conf.get('Mptool4PC', 'TN4CIOIP')
+            print("--------------------------------------------------------------tn4cioip:", tn4cioip)
         tmp = str(random.randint(1,1000))
         network.url = "http://"+tn4cioip+"/tn4cio/srv/copies_NGxx/app.php/update_NGxx_mac_to_database/"+tmp
         print("--------------------------------------------------------------url:",network.url)
