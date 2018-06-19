88defb85ea596b47054a56bc731cd97bcc80ab68
zgp-github <zgpgoole@gmail.com>
2018-06-19 15:27:38 +0800

    add for gcl

--- mptool_pyqt5/station_gcl/gcl.py
+++ mptool_pyqt5/station_gcl/gcl.py
@@ -11,6 +11,7 @@ from PyQt5.QtCore import QEvent, QTimer
 import threading
 from threading import Timer
 from time import *
+import re
 
 class GCL(QDialog):
     def __init__(self, parent=None):
@@ -25,6 +26,11 @@ class GCL(QDialog):
         mainLayout.addWidget(self.gridGroupBox)
         mainLayout.addWidget(self.QGroupBox_info_show)
         self.setLayout(mainLayout)
+        self.init_data()
+
+    def init_data(self):
+        self.GCLID_START_STATUS = False
+        self.gcl_array = []
 
     def create_cmd_input(self):
         self.gridGroupBox = QGroupBox("命令输入区")
@@ -41,7 +47,7 @@ class GCL(QDialog):
         layout.addWidget(self.cmd_input, 1, 1)
         self.cmd_input.returnPressed.connect(self.handle_cmd)
 
-        self.table = QTableWidget(2, 2)
+        self.table = QTableWidget(3, 2)
         # auto adapt the width
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
 
@@ -61,6 +67,11 @@ class GCL(QDialog):
         self.table.setItem(1, 0, newItem)
         newItem = QTableWidgetItem("None")
         self.table.setItem(1, 1, newItem)
+
+        newItem = QTableWidgetItem("入箱数量")
+        self.table.setItem(2, 0, newItem)
+        newItem = QTableWidgetItem("0")
+        self.table.setItem(2, 1, newItem)
 
         layout.addWidget(self.table, 0, 2, 4, 1)
         layout.setColumnStretch(1, 70)
@@ -85,10 +96,55 @@ class GCL(QDialog):
     def handle_cmd(self):
         cmd = self.cmd_input.text()
         print("get cmd:", cmd)
-
         self.cmd_input.clear()
+
         if cmd == "GCLID_START":
             print("get cmd: GCLID_START")
+            self.GCLID_START_STATUS = True
+            self.gcl_info_show.setText("启动入箱,请扫描需要入箱的传感器MAC地址")
+        elif cmd == "GCLID_END":
+            print("get cmd: GCLID_END")
+        elif self.GCLID_START_STATUS == True:
+            mac =  cmd
+            check = self.mac_check(mac)
+            if check == False:
+                self.gcl_info_show.setText("无效的MAC地址:"+mac)
+            else:
+                check2 = self.mac_in_gcl_array(mac)
+                if check2 == True:
+                    m = QTableWidgetItem(mac)
+                    self.table.setItem(0, 1, m)
+                    self.gcl_array.append(mac)
+                    count = len(self.gcl_array)
+                    tmp = QTableWidgetItem(str(count))
+                    self.table.setItem(2, 1, tmp)
+                    self.gcl_info_show.setText("MAC地址:"+mac)
+                else:
+                    pass
         else:
             print("cmd:"+cmd+" not support")
             self.gcl_info_show.setText("命令:"+cmd+" 不支持!")
+
+    def mac_in_gcl_array(self, mac):
+        if mac in self.gcl_array:
+            print("already exist in the gcl list")
+            self.gcl_info_show.setText("MAC:"+mac+" 已经存在当前的列表中")
+            return False
+        else:
+            return True
+        
+    def mac_check(self, addr):
+        valid = re.compile(r''' 
+            (^([0-9A-F]{1,2}[-]){5}([0-9A-F]{1,2})$ 
+            |^([0-9A-F]{1,2}[:]){5}([0-9A-F]{1,2})$ 
+            |^([0-9A-F]{1,2}[.]){5}([0-9A-F]{1,2})$
+            |^([0-9A-F]{1,2}){5}([0-9A-F]{1,2})$
+            |^([0-9A-F]{1,2}[-]){7}([0-9A-F]{1,2})$
+            |^([0-9A-F]{1,2}[:]){7}([0-9A-F]{1,2})$
+            |^([0-9A-F]{1,2}[.]){7}([0-9A-F]{1,2})$
+            |^([0-9A-F]{1,2}){7}([0-9A-F]{1,2})$) 
+            ''', re.VERBOSE | re.IGNORECASE)
+        return valid.match(addr) is not None
+
+
+
